635550c81dce33b5a60df0b2d89b6b77d7bb9d13
Ankita Paul <ankita008paul@gmail.com>
2018-07-11 13:41:52 +0530

    Profile Academic dialogs added and linked

--- app/src/main/java/in/nfly/dell/nflydemo/fragments/ProfileAcademicFragment.java
+++ app/src/main/java/in/nfly/dell/nflydemo/fragments/ProfileAcademicFragment.java
@@ -1,13 +1,16 @@
 package in.nfly.dell.nflydemo.fragments;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -52,6 +55,9 @@ public class ProfileAcademicFragment extends Fragment {
     private TextView profileXSchool,profileXBoard,profileXStream,profileXPassingyear,profileXPercentage;
     private TextView profileGitHub,profilePlayStore,profileBehance,profileBlogLink;
 
+    private ImageView workExperienceAddBtn, collegeEducationEditBtn, schoolEducationEditBtn, trainingsAddBtn,
+    projectsAddBtn, achievementsAddBtn, workSampleEditBtn;
+
     private ArrayList<String> DataSet=new ArrayList<String>(){
         {  add("blahblah");
             add("blah");
@@ -92,14 +98,248 @@ public class ProfileAcademicFragment extends Fragment {
         profileBlogLink=view.findViewById(R.id.profileBlogLink);
         profilePlayStore=view.findViewById(R.id.profilePlayStore);
 
+
+        workExperienceAddBtn=view.findViewById(R.id.workExperienceAddBtn);
+        collegeEducationEditBtn=view.findViewById(R.id.collegeEducationEditBtn);
+        schoolEducationEditBtn=view.findViewById(R.id.schoolEducationEditBtn);
+        trainingsAddBtn=view.findViewById(R.id.trainingsAddBtn);
+        projectsAddBtn=view.findViewById(R.id.projectsAddBtn);
+        achievementsAddBtn=view.findViewById(R.id.achievementsAddBtn);
+        workSampleEditBtn=view.findViewById(R.id.workSampleEditBtn);
+
+
         setWorkExperience(view);
         setTrainings(view);
         setProjects(view);
         setAchievements(view);
 
         setValues();
+        workExperienceAddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater layoutInflater=getLayoutInflater();
+                View workExperienceAddLayout=layoutInflater.inflate(R.layout.dialog_edit_work_experience,null);
+
+                AlertDialog.Builder alertDialog=new AlertDialog.Builder(getActivity());
+                alertDialog.setView(workExperienceAddLayout);
+                alertDialog.setCancelable(false);
+
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alertDialog.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Submit clicked", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                AlertDialog alert=alertDialog.create();
+                alert.show();
+
+            }
+        });
+
+        collegeEducationEditBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater layoutInflater=getLayoutInflater();
+                View collegeEducationEditLayout=layoutInflater.inflate(R.layout.dialog_edit_college_education,null);
+
+                AlertDialog.Builder alertDialog=new AlertDialog.Builder(getActivity());
+                alertDialog.setView(collegeEducationEditLayout);
+                alertDialog.setCancelable(false);
+
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alertDialog.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Submit clicked", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                AlertDialog alert=alertDialog.create();
+                alert.show();
+
+            }
+        });
+
+        schoolEducationEditBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater layoutInflater=getLayoutInflater();
+                View schoolEducationEditLayout=layoutInflater.inflate(R.layout.dialog_edit_school_education,null);
+
+                AlertDialog.Builder alertDialog=new AlertDialog.Builder(getActivity());
+                alertDialog.setView(schoolEducationEditLayout);
+                alertDialog.setCancelable(false);
+
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alertDialog.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Submit clicked", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                AlertDialog alert=alertDialog.create();
+                alert.show();
+
+            }
+        });
+
+        trainingsAddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater layoutInflater=getLayoutInflater();
+                View trainingsAddLayout=layoutInflater.inflate(R.layout.dialog_edit_trainings,null);
+
+                AlertDialog.Builder alertDialog=new AlertDialog.Builder(getActivity());
+                alertDialog.setView(trainingsAddLayout);
+                alertDialog.setCancelable(false);
+
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alertDialog.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Submit clicked", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                AlertDialog alert=alertDialog.create();
+                alert.show();
+
+            }
+        });
+
+        projectsAddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater layoutInflater=getLayoutInflater();
+                View projectsAddLayout=layoutInflater.inflate(R.layout.dialog_edit_projects,null);
+
+                AlertDialog.Builder alertDialog=new AlertDialog.Builder(getActivity());
+                alertDialog.setView(projectsAddLayout);
+                alertDialog.setCancelable(false);
+
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alertDialog.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Submit clicked", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                AlertDialog alert=alertDialog.create();
+                alert.show();
+
+            }
+        });
+
+        achievementsAddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater layoutInflater=getLayoutInflater();
+                View achievementsAddLayout=layoutInflater.inflate(R.layout.dialog_edit_achievements,null);
+
+                AlertDialog.Builder alertDialog=new AlertDialog.Builder(getActivity());
+                alertDialog.setView(achievementsAddLayout);
+                alertDialog.setCancelable(false);
+
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alertDialog.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Submit clicked", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                AlertDialog alert=alertDialog.create();
+                alert.show();
+
+            }
+        });
+
+        workSampleEditBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater layoutInflater=getLayoutInflater();
+                View workSampleEditLayout=layoutInflater.inflate(R.layout.dialog_edit_work_experience,null);
+
+                AlertDialog.Builder alertDialog=new AlertDialog.Builder(getActivity());
+                alertDialog.setView(workSampleEditLayout);
+                alertDialog.setCancelable(false);
+
+                alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                alertDialog.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "Submit clicked", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                AlertDialog alert=alertDialog.create();
+                alert.show();
+
+            }
+        });
+
+
+
+
+
 
         return view;
+
+
+
     }
 
     private void setValues() {
@@ -169,7 +409,7 @@ public class ProfileAcademicFragment extends Fragment {
     {
 
         achievementsRecyclerView=v.findViewById(R.id.profileAcademicAchievementsRecyclerView);
-        layoutManager=new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
+        layoutManager=new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL,false);
         achievementsRecyclerView.setLayoutManager(layoutManager);
 
         adapter= new ProfileAcademicAchievementsAdapter(DataSet,DataSet);
@@ -181,7 +421,7 @@ public class ProfileAcademicFragment extends Fragment {
     {
 
         workExperienceRecyclerView=v.findViewById(R.id.profileAcademicWorkExperienceRecyclerView);
-        layoutManager=new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
+        layoutManager=new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL,false);
         workExperienceRecyclerView.setLayoutManager(layoutManager);
 
         adapter= new ProfileAcademicWorkExperienceAdapter(DataSet,DataSet,DataSet,DataSet,DataSet,DataSet);
@@ -192,7 +432,7 @@ public class ProfileAcademicFragment extends Fragment {
     {
 
         trainingsRecyclerView=v.findViewById(R.id.profileAcademicTrainingsRecyclerView);
-        layoutManager=new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
+        layoutManager=new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL,false);
         trainingsRecyclerView.setLayoutManager(layoutManager);
 
         adapter= new ProfileAcademicTrainingsAdapter(DataSet,DataSet,DataSet,DataSet);
@@ -203,7 +443,7 @@ public class ProfileAcademicFragment extends Fragment {
     private void setProjects(View v)
     {
         projectsRecyclerView=v.findViewById(R.id.profileAcademicProjectsRecyclerView);
-        layoutManager=new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
+        layoutManager=new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL,false);
         projectsRecyclerView.setLayoutManager(layoutManager);
 
         adapter= new ProfileAcademicProjectsAdapter(DataSet,DataSet,DataSet);
