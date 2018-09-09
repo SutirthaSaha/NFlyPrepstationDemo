63cc03044873558afb04b1f81118c2a693a30c8b
SutirthaSaha <sutirthasaha12@gmail.com>
2018-09-09 21:37:36 +0530

    Shuffling  Pushed

--- app/src/main/java/in/nfly/dell/nflydemo/activities/CompanyWisePrepActivity.java
+++ app/src/main/java/in/nfly/dell/nflydemo/activities/CompanyWisePrepActivity.java
@@ -123,6 +123,7 @@ public class CompanyWisePrepActivity extends AppCompatActivity {
             }
         });
     }
+    
     @Override
     protected void onDestroy() {
         super.onDestroy();
